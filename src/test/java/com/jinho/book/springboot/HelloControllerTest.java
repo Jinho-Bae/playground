a9585618d4be47bc48ee162d870af1e3eea1053d
baeji <baejin922@gmail.com>
2022-06-18 13:49:26 +0900

    [2장] 테스트 코드 작성

---
+++ src/test/java/com/jinho/book/springboot/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.jinho.book.springboot;
+
+import com.jinho.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)    // SpringBoot test와 JUnit 사이에 연결자 역할을 하는 runner
+@WebMvcTest(controllers = HelloController.class)    // Web에 집중할 수 있는 annotation.
+public class HelloControllerTest {
+
+    @Autowired  // Spring이 관리하는 Bean 주입 받음
+    private MockMvc mvc;    // web API를 테스트할 때 사용
+    @Test
+    public void returnHello() throws Exception {
+        String hello = "hello";
+
+        // MockMvc를 통해 /hello 주소로 HTTP GET 요청
+        // .andExcept : mv.perform 결과 HTTP Header의 Status, body의 content 검증
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
